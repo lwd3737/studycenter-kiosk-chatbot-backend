06c2ef3f2ef9eef94e07893f51aa73cabf41c46b
lwd3737 <36585925+lwd3737@users.noreply.github.com>
2023-05-16 02:06:34 +0900

    [add]payment domain objects

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Render } from '@nestjs/common';
 
 @Controller('payment')
 export class PaymentController {
-  @Get()
-  @Render('payment/index')
-  index() {
-    return {
-      price: '10,000',
-    };
-  }
+  // @Get()
+  // @Render('payment/index')
+  // index() {
+  //   return {
+  //     price: '10,000',
+  //   };
+  // }
+
+  @Get('virtual_account')
+  async issueVirtualAccount() {}
 }
