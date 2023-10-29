73a9b53706319baeccae46f6927bc11e02d4374c
lwd3737 <36585925+lwd3737@users.noreply.github.com>
2023-10-30 00:51:46 +0900

    feature: get available my ticket usecase

--- src/modules/kakao-chatbot/controllers/kakao-chatbot-checkin-out.controller.ts
+++ src/modules/kakao-chatbot/controllers/kakao-chatbot-checkin-out.controller.ts
@@ -1,15 +1,54 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Public } from 'src/modules/auth/decorators/public.decorator';
-import { CheckInOutService } from 'src/modules/my-page';
+import { GetAvailableMyTicketsUseCase } from '../application/usecases/check-in-out/get-available-my-tickets/get-available-my-tickets.usecase';
+import { ParseAppUserIdParamPipe } from '../application/pipes/parse-app-user-id-param.pipe';
+import { DomainError, UnknownError } from 'src/core';
+import { AlreadyInUseMyTicketError } from '../application/usecases/check-in-out/get-available-my-tickets/get-available.my-tickets.error';
+import { ErrorDTOCreator } from '../application/dtos/error.dto';
+import { KAKAO_CHATBOT_PREFIX } from './controller-prefix';
+import { KaKaoChatbotResponseMapper } from '../infra/mappers/kakao-chatbot-response.mapper';
+import { ItemCardCarouselMapper } from '../infra/mappers/item-card-carousel.mapper';
 
 // TODO: public 제거
 @Public()
-@Controller('check-in-out')
-export class KakaoChatbotCheckinOutController {
-  constructor(private checkInOutService: CheckInOutService) {}
+@Controller(`${KAKAO_CHATBOT_PREFIX}`)
+export class KakaoChatbotCheckInOutController {
+  constructor(
+    private getAvailableMyTicketsUseCase: GetAvailableMyTicketsUseCase,
+  ) {}
 
   @Post('available-my-tickets')
-  async getAvailableMyTickets() {}
+  async getAvailableMyTickets(
+    @Body(new ParseAppUserIdParamPipe()) appUserId: string,
+  ) {
+    const carouselOrError = await this.getAvailableMyTicketsUseCase.execute({
+      appUserId,
+    });
+    if (carouselOrError.isErr()) {
+      console.debug(carouselOrError.error);
+
+      switch (carouselOrError.error.constructor) {
+        case AlreadyInUseMyTicketError:
+          return ErrorDTOCreator.toSimpleTextOutput(
+            '이미 사용중인 이용권이 있습니다',
+          );
+        case DomainError:
+        case UnknownError:
+        default:
+          return ErrorDTOCreator.toSimpleTextOutput(
+            '이용권 조회에 실패했습니다. 다시 시도해주세요',
+          );
+      }
+    }
+
+    return KaKaoChatbotResponseMapper.toDTO({
+      outputs: [
+        {
+          carousel: ItemCardCarouselMapper.toDTO(carouselOrError.value),
+        },
+      ],
+    });
+  }
 
   @Post('check-in')
   async checkIn() {}
